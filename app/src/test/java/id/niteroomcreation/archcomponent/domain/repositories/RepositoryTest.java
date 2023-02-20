91b5bc93b7804a5099a8e6082e8d1e53c1cc39fb
Septian Adi Wijaya <developadi@gmail.com>
2023-02-20 14:32:20 +0700

    unused

--- app/src/test/java/id/niteroomcreation/archcomponent/domain/repositories/RepositoryTest.java
+++ app/src/test/java/id/niteroomcreation/archcomponent/domain/repositories/RepositoryTest.java
@@ -1,7 +1,6 @@
 package id.niteroomcreation.archcomponent.domain.repositories;
 
 import androidx.arch.core.executor.testing.InstantTaskExecutorRule;
-import androidx.lifecycle.MutableLiveData;
 import androidx.paging.DataSource;
 import androidx.paging.PagedList;
 
@@ -17,11 +16,9 @@ import java.util.List;
 
 import id.niteroomcreation.archcomponent.domain.DummyData;
 import id.niteroomcreation.archcomponent.domain.data.local.LocalDataSource;
-import id.niteroomcreation.archcomponent.domain.data.local.entity.FavouriteEntity;
 import id.niteroomcreation.archcomponent.domain.data.local.entity.MovieEntity;
 import id.niteroomcreation.archcomponent.domain.data.local.entity.TvShowEntity;
 import id.niteroomcreation.archcomponent.util.AppExecutors;
-import id.niteroomcreation.archcomponent.util.testing.LiveDataTestUtil;
 import id.niteroomcreation.archcomponent.util.vo.Resource;
 import id.niteroomcreation.archcomponent.utils.PagedListUtil;
 
@@ -41,19 +38,11 @@ public class RepositoryTest {
 
     private FakeRepository repo;
     private List<MovieEntity> moviesResponse;
-    private List<TvShowEntity> tvShowsResponse;
-
-    private int movieId;
-    private int tvShowId;
 
     @Before
     public void setup() {
         repo = new FakeRepository(remote, localDataSource, appExecutors);
         moviesResponse = DummyData.generateRemoteDummyMovies();
-        tvShowsResponse = DummyData.generateRemoteDummyTvShows();
-
-        movieId = moviesResponse.get(0).getId();
-        tvShowId = tvShowsResponse.get(0).getId();
     }
 
     @Test
@@ -64,56 +53,8 @@ public class RepositoryTest {
         repo.getMovies();
         Resource<PagedList<MovieEntity>> me = Resource.success(PagedListUtil.mockPagedList(DummyData.constructMovies()));
         Mockito.verify(localDataSource).getMovies();
-        Assert.assertNotNull(me.data);
-        Assert.assertEquals(moviesResponse.size(), me.data.size());
+        Assert.assertNotNull(me.getData());
+        Assert.assertEquals(moviesResponse.size(), me.getData().size());
     }
 
-    @Test
-    public void getTvShows() {
-
-        DataSource.Factory<Integer, TvShowEntity> ds = Mockito.mock(DataSource.Factory.class);
-        Mockito.when(localDataSource.getTvShows()).thenReturn(ds);
-        repo.getTvShows();
-        Resource<PagedList<TvShowEntity>> me = Resource.success(PagedListUtil.mockPagedList(DummyData.constructTvShow()));
-        Mockito.verify(localDataSource).getTvShows();
-        Assert.assertNotNull(me.data);
-        Assert.assertEquals(tvShowsResponse.size(), me.data.size());
-    }
-
-    @Test
-    public void getFavourites() {
-        DataSource.Factory<Integer, FavouriteEntity> ds = Mockito.mock(DataSource.Factory.class);
-        Mockito.when(localDataSource.getFavourites()).thenReturn(ds);
-        repo.getFavourites();
-        PagedList<FavouriteEntity> me = PagedListUtil.mockPagedList(DummyData.constructFavourites());
-        Mockito.verify(localDataSource).getFavourites();
-        Assert.assertNotNull(me);
-        Assert.assertEquals(6, me.size());
-    }
-
-    @Test
-    public void getMovieById() {
-
-        Mockito.when(localDataSource.getMovieById(movieId))
-                .thenReturn(DummyData.getMovieById(movieId));
-
-        MovieEntity me = LiveDataTestUtil.getValue(repo.getMovieById(movieId));
-        Mockito.verify(localDataSource).getMovieById(movieId);
-
-        Assert.assertNotNull(me);
-        Assert.assertEquals(moviesResponse.get(0).getName(), me.getName());
-    }
-
-    @Test
-    public void getTvShowById() {
-
-        Mockito.when(localDataSource.getTvById(tvShowId))
-                .thenReturn(DummyData.getTvShowById(tvShowId));
-
-        TvShowEntity te = LiveDataTestUtil.getValue(repo.getTvShowById(tvShowId));
-        Mockito.verify(localDataSource).getTvById(tvShowId);
-
-        Assert.assertNotNull(te);
-        Assert.assertEquals(tvShowsResponse.get(0).getName(), te.getName());
-    }
 }
